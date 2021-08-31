16fcaf720b7f69695f3c543abeebd94aab4d1916
Crowdbotics <team@crowdbotics.com>
2021-08-31 11:57:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "khota_kata_cha_30219Identifier",
+  url: "https://khota_kata_cha_30219Identifier.botics.co",
+  api: "https://khota_kata_cha_30219Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
